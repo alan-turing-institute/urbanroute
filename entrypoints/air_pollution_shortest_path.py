05653adbe7625513212d9c45d6936acce4a93643
PatrickOHara <pohara@turing.ac.uk>
2020-06-16 12:34:31 +0100

    🎉 setup repo structure

---
+++ entrypoints/air_pollution_shortest_path.py
@@ -0,0 +1,11 @@
+"""Find the least cost path from source to target by minimising air pollution."""
+
+import routex as rx
+import urbanroute as ur
+
+def main():
+    """Main method for entrypoint."""
+    pass
+
+if __name__ == "__main__":
+    main()
