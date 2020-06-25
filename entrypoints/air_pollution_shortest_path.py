4637a14bd67c5a008da273fb6d1859910cb6374c
JamesCraster <jamesvaughancraster@gmail.com>
2020-06-25 17:52:36 +0100

    got shortest path working

--- entrypoints/air_pollution_shortest_path.py
+++ entrypoints/air_pollution_shortest_path.py
@@ -5,10 +5,12 @@ import logging
 import typer
 import geopandas as gpd
 import osmnx as ox
+import matplotlib.pyplot as plt
 import networkx as nx
 import math
 from cleanair.databases.queries import AirQualityResultQuery
 from cleanair.loggers import get_logger
+from shapely.geometry import Polygon
 
 from routex.types import Node
 from urbanroute.geospatial import update_cost, ellipse_bounding_box
@@ -18,9 +20,9 @@ from urbanroute.queries import HexGridQuery
 def main(  # pylint: disable=too-many-arguments
     secretfile: str,
     instance_id: str = "d5e691ef9a1f2e86743f614806319d93e30709fe179dfb27e7b99b9b967c8737",
-    source: Tuple[float, float] = (-0.1215,51.4929),
+    source: Tuple[float, float] = (51.4929,-0.1215),
     start_time: Optional[str] = "2020-01-24T09:00:00",
-    target: Tuple[float, float] = (1,2),
+    target: Tuple[float, float] = (51.4929,-0.2215),
     upto_time: Optional[str] = "2020-01-24T10:00:00",
     verbose: Optional[bool] = False,
 ):
@@ -53,30 +55,24 @@ def main(  # pylint: disable=too-many-arguments
     # gdf = gpd.GeoDataFrame(result_df, crs=4326, geometry="geom")
     logger.info("%s rows in hexgrid results", len(gdf))
 
-    G: nx.MultiDiGraph = ox.graph_from_address("British Library")
-    ox.plot_graph(G)
     if source is not None and target is not None:
-        #snap source and target to the graph
-        minimum = 1000
-        newSource = 0
-        for i,x in enumerate(G.nodes(data=True)):
-            dist = distance(x[1]['x'], x[1]['y'], source[0], source[1])
-            if(dist < minimum):
-                minimum = dist
-                newSource = x
-        minimum = 1000
-        newTarget = 0
-        for i,x in enumerate(G.nodes(data=True)):
-            dist = distance(x[1]['x'], x[1]['y'], target[0], target[1])
-            if(dist < minimum):
-                minimum = dist
-                newTarget = x
-        print(newSource,newTarget)
-
         #use bounding box of surrounding ellipse to limit graph size
-        box = ellipse_bounding_box((newSource[1]['x'],newSource[1]['y']),(newTarget[1]['x'],newTarget[1]['y']))
-    
+        box = ellipse_bounding_box((source[1],source[0]),(target[1],target[0]))
+        G: nx.MultiDiGraph = ox.graph_from_bbox(box[0],box[1],box[2],box[3])
+        G = G.to_directed()
+        
+        #snap source and target to the graph
+        newSource = ox.distance.get_nearest_node(G,source)
+        newTarget = ox.distance.get_nearest_node(G,target)
+
+        fig, ax = ox.plot_graph(G, show=False, close=False)
+        ax.scatter(source[1], source[0], c='red')
+        ax.scatter(target[1], target[0], c='red')
+        ox.plot_graph_route(G,nx.shortest_path(G, newSource, newTarget))
+        print(newSource,newTarget)
+        
         #use this bounding box to reduce size of postgis request for hexagons
+    
     logger.info(
         "%s nodes and %s edges in graph.", G.number_of_nodes(), G.number_of_edges()
     )
