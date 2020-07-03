086ee3a9661197ab4920d6fba495c7db7fa4053b
JamesCraster <jamesvaughancraster@gmail.com>
2020-07-03 11:51:52 +0100

    fixed linting

--- entrypoints/air_pollution_shortest_path.py
+++ entrypoints/air_pollution_shortest_path.py
@@ -1,49 +1,39 @@
 """Find the least cost path from source to target by minimising air pollution."""
 
-from typing import Optional, Tuple
+from typing import Tuple, Optional
 import logging
 import typer
 import geopandas as gpd
 import osmnx as ox
 import networkx as nx
-import math
-from cleanair.databases.queries import AirQualityResultQuery
+from fastapi import FastAPI
 from cleanair.loggers import get_logger
-from routex.types import Node
 from urbanroute.geospatial import update_cost, ellipse_bounding_box
 from urbanroute.queries import HexGridQuery
-from fastapi import FastAPI
 
-app = FastAPI()
+APP = FastAPI()
 
 
 def return_route(
     secretfile: str,
     instance_id,
-    source_lat: float,
-    source_long: float,
+    source: Tuple[float, float],
     start_time: str,
-    target_lat: float,
-    target_long: float,
+    target: Tuple[float, float],
     upto_time: str,
     verbose: bool = True,
 ):
     """
     Find the least polluted path.
-    
     secretfile: Path to the database secretfile.
     instance_id: Id of the air quality trained model.
-    sourceLat: latitude of the source point.
-    sourceLong: longitude of the source point.
-    targetLat: latitude of the target point.
-    targetLong: longitude of the target point.
+    source: (source latitude, source longitude) for source point.
+    target: (target latitude, target longitude) for target point.
     verbose: enable debug logging.
     """
-    source = (source_lat, source_long)
-    target = (target_lat, target_long)
     logger = get_logger("Shortest path entrypoint")
-
-    # TODO change this to a AirQualityResultQuery
+    if verbose:
+        logger.setLevel(logging.DEBUG)
     result_query = HexGridQuery(secretfile=secretfile)
     logger.info("Querying results from an air quality model")
     result_sql = result_query.query_results(
@@ -63,26 +53,25 @@ def return_route(
     # gdf = gpd.GeoDataFrame(result_df, crs=4326, geometry="geom")
     logger.info("%s rows in hexgrid results", len(gdf))
 
-    if source is not None and target is not None:
-        # use bounding box of surrounding ellipse to limit graph size
-        box = ellipse_bounding_box((source[1], source[0]), (target[1], target[0]))
-        G: nx.MultiDiGraph = ox.graph_from_bbox(box[0], box[1], box[2], box[3])
+    # use bounding box of surrounding ellipse to limit graph size
+    box = ellipse_bounding_box((source[1], source[0]), (target[1], target[0]))
+    G: nx.MultiDiGraph = ox.graph_from_bbox(box[0], box[1], box[2], box[3])
+    logger.info(
+        "%s nodes and %s edges in graph.", G.number_of_nodes(), G.number_of_edges()
+    )
 
-        # snap source and target to the graph
-        newSource = ox.distance.get_nearest_node(G, source)
-        newTarget = ox.distance.get_nearest_node(G, target)
+    logger.info("Mapping air quality predictions to the road network.")
+    G = update_cost(G, gdf, cost_attr="NO2_mean", weight_attr="length")
+    logger.debug("Printing basic stats for the graph:")
+    logger.debug(ox.stats.basic_stats(G))
 
-        logger.info(
-            "%s nodes and %s edges in graph.", G.number_of_nodes(), G.number_of_edges()
-        )
-
-        logger.info("Mapping air quality predictions to the road network.")
-        G = update_cost(G, gdf, cost_attr="NO2_mean", weight_attr="length")
-        logger.debug("Printing basic stats for the graph:")
-        logger.debug(ox.stats.basic_stats(G))
-
-        route = nx.dijkstra_path(G, newSource, newTarget, weight="NO2_mean")
-        return [G.nodes[r] for r in route]
+    route = nx.dijkstra_path(
+        G,
+        ox.distance.get_nearest_node(G, source),
+        ox.distance.get_nearest_node(G, target),
+        weight="NO2_mean",
+    )
+    return [G.nodes[r] for r in route]
 
 
 def main(  # pylint: disable=too-many-arguments
@@ -105,14 +94,21 @@ def main(  # pylint: disable=too-many-arguments
     ),
     verbose: Optional[bool] = typer.Option(False, help="Output debug logs.",),
 ):
+    """
+    secretfile: Path to the database secretfile.
+    instance_id: Id of the air quality trained model.
+    sourceLat: latitude of the source point.
+    sourceLong: longitude of the source point.
+    targetLat: latitude of the target point.
+    targetLong: longitude of the target point.
+    verbose: enable debug logging.
+    """
     return_route(
         secretfile,
         instance_id,
-        source_lat,
-        source_long,
+        (source_lat, source_long),
         start_time,
-        target_lat,
-        target_long,
+        (target_lat, target_long),
         upto_time,
         verbose,
     )
@@ -122,10 +118,17 @@ if __name__ == "__main__":
     typer.run(main)
 
 
-@app.get("/route/")
+@APP.get("/route/")
 async def get_route(
     source_lat: float, source_long: float, target_lat: float, target_long: float
 ):
+    """
+    API route to get route from A to B.
+    sourceLat: latitude of the source point.
+    sourceLong: longitude of the source point.
+    targetLat: latitude of the target point.
+    targetLong: longitude of the target point.
+    """
     secretfile: str = "/home/james/clean-air-infrastructure/.secrets/db_secrets_ad.json"
     instance_id: str = "d5e691ef9a1f2e86743f614806319d93e30709fe179dfb27e7b99b9b967c8737"
     start_time: Optional[str] = "2020-01-24T09:00:00"
@@ -133,10 +136,8 @@ async def get_route(
     return return_route(
         secretfile,
         instance_id,
-        source_lat,
-        source_long,
+        (source_lat, source_long),
         start_time,
-        target_lat,
-        target_long,
+        (target_lat, target_long),
         upto_time,
     )
--- urbanroute/urbanroute/geospatial/ellipses.py
+++ urbanroute/urbanroute/geospatial/ellipses.py
@@ -1,6 +1,9 @@
+"""
+    Use the ellipse bounding box method in order to bound two points.
+    An Optimum Vehicular Path Solution with Multi-heuristics, Feng Lu and Yanning Guan
+"""
 import math
 from typing import Tuple, List, Optional
-import osmnx as ox
 
 
 def ellipse_bounding_box(
@@ -13,20 +16,20 @@ def ellipse_bounding_box(
 
     Args:
         source, target: two points in longitude, latitude format
-        tau: a constant to be multiplied by the distance between the source and target; the optimal path
-                is assumed to be smaller than this product.
-    
-    Returns: 
+        tau: a constant to be multiplied by the distance between the source and target;
+        the optimal path is assumed to be smaller than this product.
+
+    Returns:
         A bounding box that surrounds the source and target, in north, south, east, west format,
         includes all paths of length at most: tau multiplied by the distance between the points.
-        This inclusion is done by creating an implicit elipse that the source and target are foci of.
+        This inclusion is done by creating an implicit ellipse
+        that the source and target are foci of.
     """
     if (target[0] - source[0]) == 0:
         theta = math.pi / 2
     else:
         theta = math.atan((target[1] - source[1]) / (target[0] - source[0]))
     tau = 1.1
-    distance = ox.distance.great_circle_vec(source[1], source[0], target[1], target[0])
     center_latitude = (target[1] + source[1]) / 2
     center_longitude = (target[0] + source[0]) / 2
     # here we define an ellipse with the source and target points as foci
