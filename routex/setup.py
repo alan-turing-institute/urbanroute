9fe6cbf2330ca564e299858ffb2065827f24326c
JamesCraster <jamesvaughancraster@gmail.com>
2020-08-12 15:56:12 +0100

    added graph-tool to travis

--- routex/setup.py
+++ routex/setup.py
@@ -14,6 +14,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["graph-tool==2.11"],
+    install_requires=[],
     python_requires=">=3.6",
 )
