f5a12e2f0d4beb947b7df574de742d50f5e74ee9
JamesCraster <jamesvaughancraster@gmail.com>
2020-08-21 15:22:40 +0100

    neatened up mospp, added more comments

--- routex/routex/mospp.py
+++ routex/routex/mospp.py
@@ -5,12 +5,16 @@ from graph_tool.all import Vertex, EdgePropertyMap
 
 
 class Label:
-    """The label class, contains predecessor of label, resource costs, and the associated vertex"""
+    """The label class, contains the label predecessor of the label, resource costs, and the associated vertex"""
 
     def __init__(self, pred, resource: np.ndarray, assoc: Vertex):
+        # predecessor label
         self.pred = pred
+        # array of resource values
         self.resource = resource
+        # associated vertex
         self.assoc = assoc
+        # true if it has been excluded
         self.removed = False
 
     def __gt__(self, other):
@@ -28,11 +32,15 @@ def mospp(
 ):
     """Run MOSPP on graph. Returns list of routes, each route being a list of vertices"""
     labels = [Label(None, np.array([0, 0]), source)]
+    # labels associated with each vertex
     vertex_dict = {}
     while len(labels) != 0:
+        # pick lexicographically smallest label if it isn't
+        # already excluded
         current = heapq.heappop(labels)
         if not current.removed:
             for out_edge in current.assoc.out_edges():
+                # create the new label with updated resource values
                 new_label = Label(
                     current,
                     [
@@ -41,27 +49,37 @@ def mospp(
                     ],
                     out_edge.target(),
                 )
+                # check if other labels exist for this vertex
                 if out_edge.target() in vertex_dict:
-                    discard = False
+                    # check if the new label is dominated
                     for vertex_label in vertex_dict[out_edge.target()]:
                         if vertex_label.dominate(new_label):
-                            discard = True
-                    for vertex_label in vertex_dict[out_edge.target()]:
-                        if new_label.dominate(vertex_label):
-                            vertex_dict[out_edge.target()].remove(vertex_label)
-                            vertex_label.removed = True
-
-                    if not discard:
+                            break
+                    else:
+                        # keep the new label as it is not dominated
                         vertex_dict[out_edge.target()].append(new_label)
                         heapq.heappush(labels, new_label)
+                        # remove labels that the new label dominates from the heap
+                        for vertex_label in vertex_dict[out_edge.target()]:
+                            if new_label.dominate(vertex_label):
+                                vertex_label.removed = True
+                        # remove such labels from association with their vertex
+                        vertex_dict[out_edge.target()][:] = [
+                            vertex_label
+                            for vertex_label in vertex_dict[out_edge.target()]
+                            if not vertex_label.removed
+                        ]
                 else:
+                    # no labels for this vertex yet, add the new label
                     vertex_dict[out_edge.target()] = [new_label]
                     heapq.heappush(labels, new_label)
+    # begin backtracking
     routes = []
     route = []
     route.append(target)
     for label in vertex_dict[target]:
         v = target
+        # keep track of our current label
         label_tracker = label
         while v != source:
             label_tracker = label_tracker.pred
