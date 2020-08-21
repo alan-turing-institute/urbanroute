f799df093112975f99479d4a6c4041ae61e9fe53
JamesCraster <jamesvaughancraster@gmail.com>
2020-08-21 15:52:22 +0100

    moved graph optimisation to seperate source file

--- entrypoints/main.py
+++ entrypoints/main.py
@@ -10,7 +10,12 @@ from graph_tool.all import load_graph, EdgePropertyMap
 from haversine import haversine
 from cleanair.loggers import get_logger
 from routex import astar, mospp
-from urbanroute.geospatial import ellipse_bounding_box, coord_match
+from urbanroute.geospatial import (
+    ellipse_bounding_box,
+    coord_match,
+    remove_leaves,
+    remove_paths,
+)
 
 APP = FastAPI()
 logger = get_logger("Shortest path entrypoint")
@@ -41,61 +46,13 @@ for e in G.edges():
     pollution[e] = float(mean[e]) * float(length[e])
 
 inside = G.new_vertex_property("bool")
-path = 0
-
 del_list = G.new_vertex_property("bool")
-new_edges = []
 for v in G.vertices():
     del_list[v] = True
-    # remove leaf nodes
-    if v.out_degree() == 0 or v.in_degree() == 0:
-        del_list[v] = False
-    # remove nodes in paths so long as they are not more
-    # than 50m away from either of their neighbours
-    if v.out_degree() == 1 and v.in_degree() == 1:
-        cut = True
-        new_edge_length = 0
-        new_edge_pollution = 0
-        into = None
-        outof = None
-        for into in v.in_edges():
-            if (
-                haversine(
-                    (pos[v].a[0], pos[v].a[1]),
-                    (pos[into.source()].a[0], pos[into.source()].a[1]),
-                    unit="m",
-                )
-                > 50
-            ):
-                cut = False
-            new_edge_length = length[into]
-            new_edge_pollution = pollution[into]
 
-        for outof in v.out_edges():
-            if (
-                haversine(
-                    (pos[v].a[0], pos[v].a[1]),
-                    (pos[outof.target()].a[0], pos[outof.target()].a[1]),
-                    unit="m",
-                )
-                > 50
-            ):
-                cut = False
-            # calculate length
-            new_edge_length = new_edge_length + length[outof]
-            new_edge_pollution = new_edge_pollution + pollution[outof]
-
-        if cut and (not into is None) and (not outof is None):
-            e = (into.source(), outof.target(), new_edge_length, new_edge_pollution)
-            new_edges.append(e)
-            # remove vertex
-            del_list[v] = False
-
-# create a new edge bridging the removed vertex
-for e in new_edges:
-    new = G.add_edge(e[0], e[1])
-    length[new] = e[2]
-    pollution[new] = e[3]
+# do graph simplification
+remove_leaves(G, del_list)
+remove_paths(G, del_list, pos, length, pollution)
 
 # set up numpy array of vertices with just the position
 vertices = G.get_vertices(vprops=[float_x, float_y])
--- urbanroute/urbanroute/geospatial/__init__.py
+++ urbanroute/urbanroute/geospatial/__init__.py
@@ -3,3 +3,4 @@
 from .intersection import update_cost
 from .ellipses import ellipse_bounding_box
 from .coord_match import coord_match
+from .simplify_graph import remove_leaves, remove_paths
