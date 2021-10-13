92b2a306ea9a906e5f8451404b17a207641769af
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2021-10-13 17:48:54 +0100

    🎨 Load from address

--- entrypoints/urbanair_graph.py
+++ entrypoints/urbanair_graph.py
@@ -1,6 +1,7 @@
 """Get a graph of the London road network with air quality data queried from the urbanair API."""
 
-from datetime import datetime
+from datetime import datetime, timezone
+from enum import Enum
 from pathlib import Path
 import requests
 
@@ -12,23 +13,49 @@ import typer
 from urbanroute import get_bounding_box, get_forecast_hexgrid_1hr_gdf
 from urbanroute import geospatial as gs
 
-def main(username: str, password: str, csv_output_dir: Path):
+class LondonaqName(str, Enum):
+    """Dataset names"""
+
+    tiny = "laqtiny"
+    kx = "laqkx"
+
+class LondonaqAddress(Enum):
+    """Addresses for datasets"""
+
+    tiny = "King's Cross"
+    kx = "King's Cross"
+
+def londonaq_tiny_dataset(username: str, password: str, csv_output_dir: Path):
     # small bounding box for testing
     lon_min=-0.125
     lat_min=51.53
     lon_max=-0.120
     lat_max=51.534
     small_bounding_box = get_bounding_box(lon_min, lat_min, lon_max, lat_max)
+    directed_multigraph = ox.graph.graph_from_bbox(lat_max, lat_min, lon_max, lon_min)
+
+
+
+def main(londonaq_name: LondonaqName, username: str, password: str, csv_output_dir: Path):
+
+    if londonaq_name == LondonaqName.tiny:
+        distance = 200
+    else:
+        distance = 5000
+    address = LondonaqAddress[londonaq_name.name].value
+    center_point = ox.geocoder.geocode(query=address)
+    north, south, east, west = ox.utils_geo.bbox_from_point(center_point, distance)
+    bounding_box = get_bounding_box(west, south, east, north)
+    directed_multigraph = ox.graph_from_bbox(north, south, east, west)
 
     # authenticate with password
     basic_auth = requests.auth.HTTPBasicAuth(username, password)
 
     # get dataframe from API request
-    time = datetime(2021, 8, 12, 6, 0, 0)
-    pollution_df = get_forecast_hexgrid_1hr_gdf(basic_auth, time, index=1, bounding_box=small_bounding_box)
+    time = datetime(2021, 10, 13, 8, 0, 0, tzinfo=timezone.utc)
+    pollution_df = get_forecast_hexgrid_1hr_gdf(basic_auth, time, index=1, bounding_box=bounding_box)
     print(pollution_df)
 
-    directed_multigraph = ox.graph.graph_from_bbox(lat_max, lat_min, lon_max, lon_min)
     # convert directed into undirected graph if the geometries of two arcs match
     undirected_multi_graph = ox.get_undirected(directed_multigraph)
     G = gs.update_cost(undirected_multi_graph, pollution_df, weight_attr="length")
@@ -46,17 +73,13 @@ def main(username: str, password: str, csv_output_dir: Path):
     # G.remove_edges_from(nx.selfloop_edges(G))
     edges_df = nx.to_pandas_edgelist(G, source="source", target="target")
     nodes_df = pd.DataFrame.from_dict(dict(G.nodes(data=True)), orient='index')
-    nodes_df = nodes_df.rename(columns={"y": "lat", "x": "lon"})
     print(edges_df.loc[edges_df.key > 0])
     print(nodes_df)
 
-    # save edges with pollution to CSV
-    LONDONAQ_TINY_EDGES_FILENAME = "londonaq_tiny_edges.csv"
-    LONDONAQ_TINY_NODES_FILENAME = "londonaq_tiny_nodes.csv"
-
     csv_output_dir.mkdir(exist_ok=True, parents=False)
-    edges_df.to_csv(csv_output_dir / LONDONAQ_TINY_EDGES_FILENAME, index=False)
-    nodes_df.to_csv(csv_output_dir / LONDONAQ_TINY_NODES_FILENAME, index=True, index_label="node")
+    prefix = londonaq_name.value + "A"
+    edges_df.to_csv(csv_output_dir / (prefix+ "_edges.csv"), index=False)
+    nodes_df.to_csv(csv_output_dir / (prefix + "_nodes.csv"), index=True, index_label="node")
 
 # output is a CSV file representing an undirected simple graph
 
